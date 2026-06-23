7dd7a5437f9c1fcdd31b1caeed50741e7d8b3e49
BlackB34rd <contact@the-tiny-spark.com>
2026-06-23 16:25:30 +0200

    add tp n°7

--- TP.md
+++ TP.md
@@ -1,10 +1,5 @@
-TP N°6
+TP N°7
 
-- Déclarer un objet possédant les informations suivantes: 
-
-- Nom <-- nom du produit
-- Prix <-- prix du produit
-- Stock <-- stock du produit
-
-Afficher les différentes informatons contenues dans l'objet 
-au sein du HTML de la balise possédant l'id = resultat.
+    - Ajouter une dizaine de prix dans un tableau
+    - Calculez-en le total HT puis envoyez le à la console
+    - Puis calculer le prix TTC (TVA 20%) et envoyez le à la console
--- src/main.js
+++ src/main.js
@@ -1,21 +1,43 @@
-function displayProduct(container, product){
-    container.innerHTML = `
-    Nom: ${product.name} <br />
-    Prix: ${product.price}€ <br />
-    Stock: ${product.stock} <br />
-    `;
+
+function addition(a,b){
+    return a + b ;
 }
 
 function onPageStarted(){
-    const product = {
-        name: "Steam Machine", 
-        price: 1039, 
-        stock: 1000000
-    }; 
+    const prices = [ 1039, 499, 269.99 ];
+    // retourne la longueur du tableau (le nombre d'éléments )
+    console.log(prices.length); 
 
-    const container = document.querySelector("#product1");
+    // ajoute un x éléments à la fin du tableau
+    prices.push(589);
+    prices.push(addition(10,5));
+    console.log(prices);
 
-    displayProduct(container, product);
+    // retire le dernier élément du tableau
+    prices.pop();
+    console.log(prices);
+
+    // ajoute x éléments au début du tableau
+    prices.unshift(1,2,3,4);
+    console.log(prices);
+    
+    // supprime le premier élément du tableau
+    prices.shift();
+    console.log(prices);
+    
+    // les tableaux ne sont pas typés en Javascript, çàd qu'ils peuvent 
+    // contenir des éléments de différents types
+    prices.push(true, "coucou");
+    console.log(prices);
+
+
+    // les boucles sont des structures de contrôle qui permettent 
+    // d'itérer (de répéter la même opération) un certain nombre 
+    // de fois en fonction d'une ou ou plusieurs conditions de sortie
+    for( let i = 0; i < 10; i++){
+        console.log(i);
+    }   
+
 }
 
 window.onload = onPageStarted;
