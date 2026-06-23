0a45f59ad9d8b8ad9ced0756d47a230c4b9f78a1
BlackB34rd <contact@the-tiny-spark.com>
2026-06-23 15:09:14 +0200

    add tp n°5 correction

--- src/main.js
+++ src/main.js
@@ -19,8 +19,16 @@ function soustraction( a, b ){
 function onPageStarted(){
     // const resultatElement = document.getElementById("resultat");
     const resultatElement = document.querySelector("#resultat");
-    const resultat = addition(10,5); 
-    resultatElement.innerHTML = resultat;
+    const resultat1 = addition(10,5); // 15
+    const resultat2 = soustraction(10,5); // 5
+    const resultat3 = multiplication(10,5);  // 50
+    const resultat4 = division(10,5);  // 2
+    resultatElement.innerHTML = `
+        ${resultat1}
+        ${resultat2}
+        ${resultat3}
+        ${resultat4}
+    `; 
 }
 
 // lance la fonction onPageStarted quand le document (page web)
