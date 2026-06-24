54ee0832208384c70205a9fcb7833ae89dc00938
BlackB34rd <contact@the-tiny-spark.com>
2026-06-24 15:53:59 +0200

    add tp n°12 correctioné

--- src/main2.js
+++ src/main2.js
@@ -1,18 +1,26 @@
 var tab = [10,20,30,40,50,60,70,80,90,100]; 
 
-// on va enlever un élément du tableau par exemple le nombre 40
-// on va le faire à l'aide de indexOf et splice
-
-
 // on récupère la position du nombre 40 à l'aide de indexOf
-const pos = tab.indexOf(40); 
+const toRemove = [40,50,80,90];
 
-if( pos > -1 )
-    tab.splice(pos, 1);
+// tant qu'il y a un numéro à supprimer
+while( toRemove.length > 0 ){
+
+    // alors on récupère le prochain à supprimer
+    // en le retirant de notre tableau
+    // pop permet de retirer et de retourner du dernier élément
+    // ce qui, à chaque tour de boucle réduit la longueur
+    // du tableau toRemove et au passage, me permet de stocker 
+    // le nombre dans la variable num
+    let num = toRemove.pop();
+
+    // on cherche au sein de notre tableau si num y est présent
+    let pos = tab.indexOf(num); 
+
+    // si oui
+    if( pos > -1)
+        tab.splice(pos, 1); //  on le retire du tableau
+
+}
 
 console.log(tab);
-
-// Exercice, essayez de supprimer les nombres 80,90 et 50 du tableau 
-// avec la même façon de faire
-
-// Exercice supplémentaire, essayez avec un nombre qui n'est pas dans le tableau
