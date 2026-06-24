d58e5ffc9dc232263f1b2522d221b4ccffe584f4
BlackB34rd <contact@the-tiny-spark.com>
2026-06-24 15:58:40 +0200

    add tp n°13

--- src/main2.js
+++ src/main2.js
@@ -1,26 +1,25 @@
 var tab = [10,20,30,40,50,60,70,80,90,100]; 
 
-// on récupère la position du nombre 40 à l'aide de indexOf
-const toRemove = [40,50,80,90];
-
-// tant qu'il y a un numéro à supprimer
-while( toRemove.length > 0 ){
-
-    // alors on récupère le prochain à supprimer
-    // en le retirant de notre tableau
-    // pop permet de retirer et de retourner du dernier élément
-    // ce qui, à chaque tour de boucle réduit la longueur
-    // du tableau toRemove et au passage, me permet de stocker 
-    // le nombre dans la variable num
-    let num = toRemove.pop();
-
-    // on cherche au sein de notre tableau si num y est présent
-    let pos = tab.indexOf(num); 
-
-    // si oui
-    if( pos > -1)
-        tab.splice(pos, 1); //  on le retire du tableau
-
+function ascSorting(){
+    // cette fonction permet de comparer les éléments du tableau
+    // 2 par 2
+    tab.sort(
+        function( a, b ){
+            // si on renvoie 1 alors, l'ordre est préservé
+            // si on renvoie -1 alors l'ordre est inversé
+            if( a < b){
+                return -1;
+            }
+            else{
+                return 1;
+            }
+        }
+    );
 }
 
-console.log(tab);
+// utilisez le bouton createProductBtn sur le index.html
+// lorsque vous cliquez sur ce bouton, alors le tableau, doit 
+// être ordonné de façon décroissante
+
+// ATTENTION n'oubliez pas de démarrer votre code dans une fonction
+// qui est appelée lorsque window.onload est lancé
