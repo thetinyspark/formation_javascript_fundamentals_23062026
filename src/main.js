05947ccf5e2c3d9a5d3425bdfc79fa31dfc0bc34
BlackB34rd <contact@the-tiny-spark.com>
2026-06-25 09:53:07 +0200

    add tp n°14 correction

--- src/main.js
+++ src/main.js
@@ -17,10 +17,32 @@ class Heroe{
     }
 }
 
+class Car{
+    constructor(param_marque, param_couleur, param_prix){
+        this.brand = param_marque; 
+        this.color = param_couleur; 
+        this.price = param_prix; 
+    }
+
+    move(){
+        console.log("vroum");
+    }
+}
+
 // mage et conan sont des objets de type Heroe
 // construits à partir de la classe Heroe
 // on dit aussi que ce sont des instances de Heroe
 const mage = new Heroe("Faire de la magie", "Robe");
 const conan = new Heroe("Taper","Tout nu");
 
-console.log(mage, conan);
+const flashMcQueen = new Car("Ferrari","rouge",500000); 
+flashMcQueen.move();
+
+// Javascript, même s'il introduit des éléments de langage orienté objet par classe 
+// est un langage orienté objet par prototypage, çàd, que ses propriétés et méthodes/fonctionnalités
+// peuvent être supprimées, redéfinies, ajoutées à n'importe quel moment. 
+// Le langage est extrêmement souple, mais la rapidité d'éxécution en pâtit. 
+flashMcQueen.introduceItSelf = function(){
+
+};
+
